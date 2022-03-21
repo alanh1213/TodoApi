0a44f940f67ec55933b170211989a256255313e3
alanh1213 <alanh1213@hotmail.com>
2022-03-21 06:18:30 -0300

    Agregar archivos de proyecto.

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+            //Constructor
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }  //El signo de pregunta es por si el campo name viene vacio --> osea null. Para que sea string y null dependiendo del caso
+        public bool IsComplete { get; set; }
+        public string? Secret { get; set; }
+    }
+}
